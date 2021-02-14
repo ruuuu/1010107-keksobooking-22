53511044bb37ab7a9ba9cab9bc1f56be2b9d2022
rufina <rufinka_91@mail.ru>
2021-02-14 22:49:53 +0300

    add swith-case operator and update createFeatureElements function

--- js/popup.js
+++ js/popup.js
@@ -2,14 +2,19 @@ import { createOffers } from  './data.js';
 
 //                                      ul
 const createFeatureElements = function (listOfFeatures, featuresArray) { // featuresArray =['wi-fi', 'wash', 'conditioner']
-      
-  if (featuresArray.length) {
-    featuresArray.forEach((elem) => { 
-      const featureElement = document.createElement('li');
-      featureElement.classList.add('popup__feature', 'popup__feature--' + elem); 
-      listOfFeatures.appendChild(featureElement);  //добавляем li в ul
-    });
-  } 
+  //1-ый способ:
+  if (featuresArray.length){
+    const features__array = featuresArray.map((feature) => `<li class="popup__feature popup__feature--${feature}"></li>`); // [<li></li>, <li></li>, <li></li>]
+    listOfFeatures.insertAdjacentHTML('beforeend', features__array.join(' ')); // строка  из элементов массива "<li></li> <li></li> <li></li>" и добавляем в родитель
+  }
+  //2-ой способ:
+  // if (featuresArray.length) {
+  //   featuresArray.forEach((elem) => { 
+  //     const featureElement = document.createElement('li');
+  //     featureElement.classList.add('popup__feature', 'popup__feature--' + elem); 
+  //     listOfFeatures.appendChild(featureElement);  //добавляем li в ul
+  //   });
+  // } 
   else {
     listOfFeatures.classList.add('hidden'); // скроем
   }
@@ -20,12 +25,15 @@ const createFeatureElements = function (listOfFeatures, featuresArray) { // feat
 const createPhotoElements = function (listOfPhotos, photosArray) {
 
   if (photosArray.length) {
-    for(let i = 0; i < photosArray.length; i++){
+    photosArray.forEach((photo) => { // функция вызывается  к каждому photo
       const photoElement = document.createElement('img');
       photoElement.classList.add('popup__photo'); 
-      photoElement.setAttribute('src', photosArray[i]); 
-      listOfPhotos.appendChild(photoElement);  //добавляем img в listOfPhotos 
-    }
+      photoElement.setAttribute('src', photo); //добавляем атрибут
+      photoElement.setAttribute('width', '45'); 
+      photoElement.setAttribute('height', '40');
+      photoElement.setAttribute('alt', 'Фотография жилья');
+      listOfPhotos.appendChild(photoElement);  //добавляем img в родитель listOfPhotos 
+    });
   }
   else{
     listOfPhotos.classList.add('hidden'); 
@@ -34,34 +42,53 @@ const createPhotoElements = function (listOfPhotos, photosArray) {
 
 
 const createTypeElem = function(typeElem, offerType){
-  if(offerType){
+  // if(offerType){
 
-    if(offerType === 'flat'){
-      offerType = 'Квартира';
-    }
+  //   if(offerType === 'flat'){
+  //     offerType = 'Квартира';
+  //   }
 
-    if(offerType === 'bungalow'){
-      offerType = 'Бунгало';
-    }
+  //   if(offerType === 'bungalow'){
+  //     offerType = 'Бунгало';
+  //   }
 
-    if(offerType === 'house'){
-      offerType = 'Дом';
-    }
+  //   if(offerType === 'house'){
+  //     offerType = 'Дом';
+  //   }
 
-    if(offerType === 'palace'){
-      offerType = 'Дворец';
-    }
+  //   if(offerType === 'palace'){
+  //     offerType = 'Дворец';
+  //   }
 
-    return  offerType;
-  }
-  else{
-    typeElem.classList.add('hidden');
-  }
+  //   return  offerType;
+  // }
+  // else{
+  //   typeElem.classList.add('hidden');
+  // }
   
+  switch(offerType) {
+    case 'flat':  
+      offerType = 'Квартира';
+      break;
+
+    case 'bungalow':  
+      offerType = 'Бунгало';
+      break;
+
+    case 'house':  
+      offerType = 'Дом';
+      break;  
+    
+    case 'palace':  
+      offerType = 'Дворец';
+      break;  
+
+    default:
+      typeElem.classList.add('hidden');
+  }
+
+  return offerType;
 };
-
-
-
 
 
 
@@ -74,6 +101,8 @@ const card = cardTemplate.querySelector('.popup'); //карточка объяв
 //console.log(card);
 
 const similarOffers = createOffers(); // [{},{},{}]
+console.log('массив объявлений ', similarOffers); //массив объектов-объявлений
+
 
 const similarListFragment = document.createDocumentFragment(); //корбка куда будем складвать оферы
 
@@ -100,7 +129,8 @@ similarOffers.forEach(( {author, offer} ) => { // для каждого офер
   offerElement.querySelector('.popup__text--time').textContent = 'Заезд после ' +  offer.checkin + ', выезд до  ' + offer.checkout;
   //console.log(offerElement.querySelector('.popup__text--time').textContent);
 
-  createFeatureElements(offerElement.querySelector('.popup__features'), offer.features); // фичи
+  
+  createFeatureElements(offerElement.querySelector('.popup__features'), offer.features); // фичи ['wi-fi', 'wash', 'conditioner']
   //console.log(offerElement.querySelector('.popup__features').textContent);
   
   offerElement.querySelector('.popup__description').textContent = offer.description;
@@ -117,3 +147,12 @@ similarOffers.forEach(( {author, offer} ) => { // для каждого офер
 });
 
 map.appendChild(similarListFragment); // вставляем коробку с оферами в родитель map
+
+
+
+
+
+
+
+
+
